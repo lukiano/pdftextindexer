0fe66c195dbac1395c70e7a351a927a8dd172f11
230980 <230980@b0d9129a-988c-11de-a20b-5bf1f98922f7>
2009-10-23 18:20:11 +0000

    Preparing for highlight

--- trunk/pdftextindexer/src/main/java/org/lucho/server/lucene/impl/IndexFilesImpl.java
+++ trunk/pdftextindexer/src/main/java/org/lucho/server/lucene/impl/IndexFilesImpl.java
@@ -2,7 +2,9 @@ package org.lucho.server.lucene.impl;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.document.Document;
@@ -12,18 +14,23 @@ import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.Field.TermVector;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.AlreadyClosedException;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParsingReader;
+import org.apache.tika.sax.BodyContentHandler;
 import org.lucho.client.Constants;
 import org.lucho.server.lucene.IndexFiles;
 import org.lucho.server.lucene.LuceneFactory;
+import org.xml.sax.SAXException;
 
 import com.google.inject.Inject;
 
 /** Index all text files under a directory. */
 public class IndexFilesImpl implements IndexFiles {
-	
+
 	private static final Logger log = Logger.getLogger(IndexFiles.class);
-	
+
 	@Inject
 	private FileFilter fileFilter;
 
@@ -51,29 +58,64 @@ public class IndexFilesImpl implements IndexFiles {
 		}
 	}
 
-	private void indexDocs(final IndexWriter writer, final File file) throws IOException {
+	private void indexDocs(final IndexWriter writer, final File file)
+			throws IOException {
 		// do not try to index files that cannot be read
 		if (file.canRead()) {
 			if (file.isDirectory()) {
 				indexFolder(writer, file);
 			} else {
-				indexFile(writer, file);
+				indexFileWithStoring(writer, file);
 			}
 		}
 	}
 
-	private void indexFile(final IndexWriter writer, final File file)
+	private void indexFileWithoutStoring(final IndexWriter writer, final File file)
 			throws IOException {
-		ParsingReader parsingReader = new ParsingReader(file);
 		Document document = new Document();
-		document.add(new Field(Constants.PATH_FIELD, file.getPath(), Store.YES, Index.NO));
-		document.add(new Field(Constants.CONTENTS_FIELD, parsingReader, TermVector.WITH_POSITIONS_OFFSETS));
+		document.add(new Field(Constants.PATH_FIELD, file.getPath(), Store.YES,
+				Index.NO));
+		ParsingReader parsingReader = new ParsingReader(file);
+		document.add(new Field(Constants.CONTENTS_FIELD, parsingReader,
+				TermVector.WITH_POSITIONS_OFFSETS));
 		try {
 			writer.addDocument(document);
 		} catch (IOException e) {
 			log.warn("Unable to index file " + file.getPath(), e);
 		} finally {
 			parsingReader.close();
+		}
+		writer.commit();
+	}
+
+	private void indexFileWithStoring(final IndexWriter writer, final File file)
+			throws IOException {
+		Document document = new Document();
+		StringWriter stringWriter = new StringWriter();
+		document.add(new Field(Constants.PATH_FIELD, file.getPath(), Store.YES,
+				Index.NO));
+		AutoDetectParser parser = new AutoDetectParser();
+		BodyContentHandler contentHandler = new BodyContentHandler(stringWriter);
+		Metadata metadata = new Metadata();
+		FileInputStream fileInputStream = new FileInputStream(file);
+		try {
+			parser.parse(fileInputStream, contentHandler, metadata);
+			document.add(new Field(Constants.CONTENTS_FIELD, stringWriter.toString(),
+					Store.YES, Index.ANALYZED, TermVector.WITH_POSITIONS_OFFSETS));
+		} catch (SAXException e) {
+			log.warn("Unable to index file " + file.getPath(), e);
+			return;
+		} catch (TikaException e) {
+			log.warn("Unable to index file " + file.getPath(), e);
+			return;
+		} finally {
+			fileInputStream.close();
+		}
+		try {
+			writer.addDocument(document);
+		} catch (IOException e) {
+			log.warn("Unable to index file " + file.getPath(), e);
+			return;
 		}
 		writer.commit();
 	}
